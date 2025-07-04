8b0330869d2d5551c71156cb837a274e6bb7b37a
anshjamwal15 <anshjamwal2002@gmail.com>
2025-07-05 04:38:43 +0530

    fix: new changes

--- src/main/java/com/dekhokaun/mindarobackend/MindaroBackendApplication.java
+++ src/main/java/com/dekhokaun/mindarobackend/MindaroBackendApplication.java
@@ -3,6 +3,9 @@ package com.dekhokaun.mindarobackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MindaroBackendApplication {
@@ -11,4 +14,17 @@ public class MindaroBackendApplication {
         SpringApplication.run(MindaroBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
